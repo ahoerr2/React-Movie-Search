e77f792410a4d3b700d98a24cb68bffc9cab70e9
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2019-12-21 22:09:52 -0600

    added favicons

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { IconContext } from "react-icons";
-import { FaGithubSquare } from "react-icons/fa";
+import { FaGithubSquare, FaReact } from "react-icons/fa";
 export default class NavBar extends Component {
   render() {
     return (
@@ -10,11 +10,35 @@ export default class NavBar extends Component {
         <Navbar className="navbar" variant="dark">
           <Navbar.Brand href="#home">Movie Search</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link href="https://github.com/ahoerr2/React-Movie-Search" target="_blank">
-            <IconContext.Provider value={{ color: "white", size: "1.20em", className: "global-class-name" }}>
-                <FaGithubSquare/>
-            </IconContext.Provider>
+            <Nav.Link
+              href="https://github.com/ahoerr2/React-Movie-Search"
+              target="_blank"
+            >
+              <IconContext.Provider
+                value={{
+                  color: "white",
+                  size: "1.20em",
+                  className: "global-class-name"
+                }}
+              >
+                <FaGithubSquare />
+              </IconContext.Provider>
               Github
+            </Nav.Link>
+            <Nav.Link 
+              href="https://reactjs.org/"
+              target="_blank"
+            >
+              <IconContext.Provider
+                value={{
+                  color: "white",
+                  size: "1.20em",
+                  className: "global-class-name"
+                }}
+              >
+                <FaReact />
+                React
+              </IconContext.Provider>
             </Nav.Link>
           </Nav>
         </Navbar>
