d17328dab1569cb0336971a4f811f5febad6e4f5
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2019-12-22 16:31:47 -0600

    Added openmdb api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import SearchRender from './components/SearchRender';
-
+import dotenv from "dotenv";
+dotenv.config();
+console.log(process.env);
 function App() {
   return (
     <div className="App">
--- src/components/layout/SearchBar.jsx
+++ src/components/layout/SearchBar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup'
 import SearchButton from './SearchButton';
+
 export default class SearchBar extends Component {
     
     constructor(props){
@@ -10,9 +11,14 @@ export default class SearchBar extends Component {
             qwery: ''
         }
         this.search = this.search.bind(this);
+        
     }
 
     search(){
+        const API_KEY = process.env.REACT_APP_API_KEY;
+        const API_LINK = process.env.REACT_APP_API_LINK;
+        const API_SEARCH = `${API_LINK}apikey=${API_KEY}&type=movie&s=${this.state.qwery}`;
+        console.log(API_SEARCH);
         this.props.onSearch(this.state.qwery);
     }
     render() {
@@ -26,7 +32,11 @@ export default class SearchBar extends Component {
                             type="search" 
                             placeholder="Enter Movie Title" 
                             onChange={event => {this.setState({qwery: event.target.value})}}
-                            onKeyPress={event => console.log('event key: ',event.key)}
+                            onKeyPress={event => {
+                                if(event.key === 'Enter'){
+                                    this.search()
+                                }
+                            }}
                             />
                             <InputGroup.Append>
                                 <SearchButton onClick={this.search}/>
