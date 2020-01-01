bc0e73c5fcce201b3f589e00c5464905dd53a4c4
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2019-12-31 18:39:26 -0600

    Added buffer functionality

--- src/components/layout/Profile.jsx
+++ src/components/layout/Profile.jsx
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 export default class Profile extends Component {
     render() {
         //let movie = {title: '', year: '', id: ''};
-        console.log(this.props.Movie);
-        const title = this.props.Movie !== undefined ? this.props.Movie.Title : "Movie Title";
-        const year = this.props.Movie !== undefined ? this.props.Movie.Year : "Movie Year";
+        if(this.props.Movie !== undefined){
+            console.log(this.props.Movie);
+        }
+        
+        const title = this.props.Movie.Title !== undefined ? this.props.Movie.Title : "Movie Title";
+        const year = this.props.Movie.Year !== undefined ? this.props.Movie.Year : "Movie Year";
+        const id = this.props.Movie.imdbID !== undefined ? this.props.Movie.imdbID : "Movie ID";
         return (
             <div>
                 <h6>{title}</h6>
                 <h6>{year}</h6>
-                <h6>Movie ID</h6>
+                <h6>{id}</h6>
             </div>
         )
     }
