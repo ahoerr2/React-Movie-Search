e63b98ad6a181ed2afcf1bc7954f881b6fdb41a6
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2019-12-31 18:51:15 -0600

    Added Poster Images to profiles

--- src/components/layout/Profile.jsx
+++ src/components/layout/Profile.jsx
@@ -2,9 +2,8 @@ import React, { Component } from 'react'
 export default class Profile extends Component {
     render() {
         //let movie = {title: '', year: '', id: ''};
-        if(this.props.Movie !== undefined){
-            console.log(this.props.Movie);
-        }
+        console.log(this.props.Movie);
+        
         
         const title = this.props.Movie.Title !== undefined ? this.props.Movie.Title : "Movie Title";
         const year = this.props.Movie.Year !== undefined ? this.props.Movie.Year : "Movie Year";
@@ -14,6 +13,14 @@ export default class Profile extends Component {
                 <h6>{title}</h6>
                 <h6>{year}</h6>
                 <h6>{id}</h6>
+                <div className="Poster">
+                    <img 
+                    className="Poster"
+                    alt="No Poster Found"
+                    src={this.props.Movie.Poster}
+                    />
+                    
+                </div>
             </div>
         )
     }
