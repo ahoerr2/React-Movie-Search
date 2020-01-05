dc56dec192f5df90b22d7253c3ddc5850fc70a12
ahoerr2 <42220471+ahoerr2@users.noreply.github.com>
2020-01-05 15:11:41 -0600

    changed circle layout to a card layout

--- src/components/SearchRender.jsx
+++ src/components/SearchRender.jsx
@@ -3,6 +3,8 @@ import NavBar from './layout/NavBar'
 import SearchBar from './layout/SearchBar.jsx';
 import Profile from './layout/Profile';
 import Spinner from 'react-bootstrap/Spinner';
+import { Container, Row, Col } from 'react-bootstrap';
+
 var imdb = require("imdb-api");
 export default class SearchRender extends Component {
     constructor(props){
@@ -28,12 +30,22 @@ export default class SearchRender extends Component {
         const API_KEY = process.env.REACT_APP_API_KEY;
         let res = await imdb.search({name: qweryr}, {apiKey: API_KEY});
         const ary = res.results;
+        for(var j = 0; j < ary.length; j++){
+            if(ary[j].type !== "movie"){
+                ary.splice(j,1);
+                j--;
+            }
+        }
         var ary2 = [];
         if(ary.length > 0){
             for(var i = 0; i < ary.length; i++){
-                ary2[i] = await imdb.get({id: res.results[i].imdbid}, {apiKey: API_KEY});
+                let movie = await imdb.get({id: res.results[i].imdbid}, {apiKey: API_KEY});
+                ary2[i] = movie; 
+                this.setState({
+                    Movie: ary2,
+                    Loading: false
+                    })
             }
-            console.log(ary2);
             this.setState({
                 Movie: ary2,
                 Loading: false
@@ -44,42 +56,6 @@ export default class SearchRender extends Component {
                 Loading: false
                 })
         }
-
-
-
-        //const res = imdb.search({name: qweryr},{apiKey: API_KEY});
-        //this.setState({
-        //    Movie: res
-        //})
-        /*qweryr = qweryr.replace(/ /g,"_");
-        let API_SEARCH = this.buildLink(qweryr,"&s");
-        let jes = null;
-        this.setState({
-            Loading: true,
-            Movie: null
-        })
-        await fetch(API_SEARCH,{
-            method: 'Get'
-        })
-        .then(Response => Response.json())
-        .then(json => {
-            this.setState({
-                Movie: json.Search
-            });
-           
-        });           
-        jes = this.state.Movie;
-        jes = jes.map(jes => this.buildLink(jes.imdbID,"&i"));
-        await fetch(jes[0],{
-            method: 'Get'
-        })
-        .then(Response=>Response.json())
-        .then(json => {
-            this.setState({
-            Movie: json,
-            Loading: false
-        })});
-        */
     }
     
 
@@ -92,17 +68,26 @@ export default class SearchRender extends Component {
                 <h1>Movie Title Database</h1>
                 <h4>By Alex Hoerr</h4>
                 <SearchBar onSearch={this.changeMovie} loading={this.state.Loading}/>
+                
                 <React.Fragment>
-                    {this.state.Movie ? 
-                    <div className="Column">
-                        {this.state.Movie.map((Movie,i) => (
-                            <Profile Movie={Movie} key={i}/>
-                        ))}
-                    </div> : 
-                    (this.state.Loading ? 
-                    <Spinner animation="border" role="status">
-                        <span className="sr-only">Loading...</span>
-                    </Spinner> : <h6>No Movies Found</h6>)}
+                    {
+                        this.state.Loading ? 
+                        <Spinner animation="border" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </Spinner> : 
+                    this.state.Movie ? 
+                    <div className="Cards">
+                        <Container>
+                            <Row>
+                        { this.state.Movie.length > 1 ?this.state.Movie.map((Movie,i) => (
+                            <Col lg="3" key={i}>
+                                <Profile Movie={Movie} key={i}/>
+                            </Col>
+                        )) : <Profile Movie={this.state.Movie} key={0}/>}
+                            </Row>
+                        </Container>
+                    </div> : <h6>No Movies Found</h6>
+                    }
                 </React.Fragment>
                 </div>
             </div>
--- src/components/layout/Profile.jsx
+++ src/components/layout/Profile.jsx
@@ -1,49 +1,54 @@
 import React, { Component } from "react";
+import {Card} from 'react-bootstrap';
 import * as url from '../images/nf.jpg';
 export default class Profile extends Component {
+  constructor(props){
+    super(props)
+    this.cutSummary = this.cutSummary.bind(this);
+  }
+
+  cutSummary(plot){
+    plot = plot.substring(0,197);
+    let space = plot.lastIndexOf(" ");
+    plot = plot.substring(0,space);
+    return plot + "...";
+  }
   render() {
-    let movie = {title: 'Title Unavaliable', year: 'Year Unavaliable', metascore: 'Score Not Found',poster:url.default}
+    let movie = {title: 'Title Unavaliable', year: 'Year Unavaliable', plot: 'No Summary Avaliable', poster:url.default}
     
     if (this.props.Movie !== null) {
         movie = this.props.Movie;
     }
-
     const title =
       movie.Title !== null
         ? movie.title
-        : "Movie Title";
+        : "Error title";
     const year =
       movie.year !== undefined
         ? "Release Year: " + movie.year
-        : "Movie Year";
-    const id =
-      movie.metascore !== undefined
-        ? "Metascore: " + movie.metascore
-        : "Metascore";
+        : "Error Year";
+    const plot =
+      movie.plot !== undefined
+        ? movie.plot = movie.plot.length < 200 ? movie.plot : this.cutSummary(movie.plot)
+        : "Error Plot";
+
         const poster =
-        movie.poster !== undefined
+        movie.poster !== "N/A"
           ? movie.poster
           : url.default;
     
+
+    const widthVar = "17rem";
+    const heightVar = "21rem";
     return (
-      <div className="profile">
-        <img
-          className="profile-img"
-          alt="error"
-          src={poster}
-        />
-        <div className="profile-atb">
-          <div className="profile-title">
-            <h6>{title}</h6>
-          </div>
-          <div className="profile-year">
-            <h6>{year}</h6>
-          </div>
-          <div className="profile-id">
-            <h6>{id}</h6>
-          </div>
-        </div>
-      </div>
+      <Card bg="light" text="dark" style={{width: widthVar}}>
+        <Card.Img variant='top' src={poster} style={{height: heightVar}}/>
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+    <Card.Subtitle>Released: {year}</Card.Subtitle>
+    <Card.Text>{plot}</Card.Text>
+        </Card.Body>
+      </Card>
     );
   }
 }
